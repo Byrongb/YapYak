4878746d88427135a9b6271c5d17620fa52cfaf9
Byron <bgguerra@scl.edu.gt>
2024-04-06 20:48:16 -0700

    saldo y buscador

---
+++ js/home.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const homeContainer = document.getElementById('home-container');
+
+    // Crear el header y sus contenidos
+    const header = document.createElement('header');
+    homeContainer.appendChild(header);
+
+    // Sección del saldo
+    const balanceInfo = document.createElement('div');
+    balanceInfo.className = 'balance-info';
+    header.appendChild(balanceInfo);
+
+    const walletIcon = document.createElement('img');
+    walletIcon.src = 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/436658333_1170170567307933_8621982789288833053_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=qzrQduE64uMAb7n77vj&_nc_ht=scontent-sea1-1.xx&oh=00_AfAPb5leQlFISdcdfC23rhjZw-ajKC-TR1SALyjG3ERj-w&oe=6617EBE7'; // Asegúrate de tener este ícono en tu proyecto
+    walletIcon.alt = 'Saldo';
+    walletIcon.className = 'wallet-icon';
+    balanceInfo.appendChild(walletIcon);
+
+    const balance = document.createElement('span');
+    balance.className = 'balance';
+    balance.textContent = '$0.00'; // Ejemplo de saldo
+    balanceInfo.appendChild(balance);
+
+    // Barra de búsqueda
+    const searchBar = document.createElement('input');
+    searchBar.type = 'search';
+    searchBar.className = 'search-bar';
+    searchBar.placeholder = 'Busca un nombre o número';
+    header.appendChild(searchBar);
+});
--- js/principal.js
+++ js/principal.js
@@ -6,12 +6,15 @@ window.onload = function() {
     logo.src = 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/434339732_1167308667594123_6935472885804797777_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=AvNIIwcOfXwAX_L1DJ4&_nc_ht=scontent-sea1-1.xx&oh=00_AfA_RUxQ3W5yqyrR-CDubnpX0o8O9senglBa4AZF0jxlDw&oe=66112A04';
     logo.alt = 'YapYak Logo';
     logo.classList.add('logo');
+    app.appendChild(logo);
 
     // Crear el título
     const title = document.createElement('h1');
     title.textContent = 'YapYak';
-
-    // Añadir el logo y el título al div #app
-    app.appendChild(logo);
     app.appendChild(title);
+
+    // Redirigir a home.html después de 4 segundos
+    setTimeout(() => {
+        window.location.href = 'home.html';
+    }, 4000);
 };

9a19beaa81e483819e802eb92ff494544ca34228
Byron <bgguerra@scl.edu.gt>
2024-04-06 21:55:28 -0700

    prueba de quickActions

--- js/principal.js
+++ js/principal.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
     // Crear el logo
     const logo = document.createElement('img');
-    logo.src = 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/434339732_1167308667594123_6935472885804797777_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=AvNIIwcOfXwAX_L1DJ4&_nc_ht=scontent-sea1-1.xx&oh=00_AfA_RUxQ3W5yqyrR-CDubnpX0o8O9senglBa4AZF0jxlDw&oe=66112A04';
+    logo.src = 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/434339732_1167308667594123_6935472885804797777_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=NP2nXsXO6fAAb6wyVHM&_nc_ht=scontent-sea1-1.xx&oh=00_AfAYxqABfkuSgmZ-aj-o1bRA0gpwjZBAW_1IAyMI7mQQJg&oe=6617F9C4';
     logo.alt = 'YapYak Logo';
     logo.classList.add('logo');
     app.appendChild(logo);
---
+++ js/quickActions.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.getElementById('quick-actions-container');
+
+    const actions = [
+        { title: 'Ver<br>contactos', icon: 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/436801839_1170198530638470_5899165096378336425_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=OUcVjIlxXIAAb7dPXRb&_nc_ht=scontent-sea1-1.xx&oh=00_AfDLuC3wwSo4Lqjw4fLY3UmvA8fPYKhInfEfLOi_QL7vBQ&oe=6617F790' },
+        { title: 'Recarga<br>móvil', icon: 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/436770755_1170195520638771_5708917345082855468_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=WXFsiyBl5PQAb4GKv0h&_nc_ht=scontent-sea1-1.xx&oh=00_AfBkTtQO1jkBGyLO0OPL8yiirqxC4dkUQJ3g7wlo28DG3Q&oe=6617FC4E' },
+        { title: 'Transferencia<br>de dinero', icon: 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/436907132_1170192953972361_3525272167220031839_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gkjImOtDqfIAb4BhUz-&_nc_ht=scontent-sea1-1.xx&oh=00_AfCiIQ_sfZmDWkJrho4XzIP1o9xV4fDG7HQ5N9nB1bPB6g&oe=6617E4A5' },
+        { title: 'Planes<br>de llamadas', icon: 'https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/436801275_1170197120638611_5243823755745497661_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=8O_7SVvfBqMAb4Szbp0&_nc_ht=scontent-sea1-1.xx&oh=00_AfBx0bYyMReXwCPiOia10FYmqh6BsI3B-p_CDFqecUCKoA&oe=661812A5' },
+    ];
+
+    actions.forEach(action => {
+        const actionButton = document.createElement('div');
+        actionButton.className = 'action-button';
+
+        const icon = document.createElement('img');
+        icon.src = action.icon;
+        icon.className = 'action-icon';
+        actionButton.appendChild(icon);
+
+        const title = document.createElement('div'); // Usamos 'div' para contener el texto
+        title.className = 'action-title';
+        title.innerHTML = action.title; // Usamos innerHTML para interpretar el HTML (como <br>)
+        actionButton.appendChild(title);
+
+        container.appendChild(actionButton);
+    });
+});
